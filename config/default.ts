aa5973019be86479b497daacc3910bb88a8a7595
ciwan <cokyamanmuhammet@gmail.com>
2022-05-12 16:49:37 +0300

    config and middleware build

---
+++ config/default.ts
@@ -0,0 +1,15 @@
+export default {
+    app: {
+        port: 3000,
+        saltWorkFactor: 10,
+        accessTokenTtl: "15m",
+        refreshTokenTtl: "1y",
+        publicKey: ``,
+        privateKey: ``,
+    },
+    db: {
+        host: "localhost",
+        name: "build-restapi-with-typescript",
+        dbUri: "mongodb+srv://ckymn:ckymn123.@cluster0.fzenv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    }
+}
