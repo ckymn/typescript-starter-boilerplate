a008b6a9768db6a8368f1fa484e531a96b3681af
ciwan <cokyamanmuhammet@gmail.com>
2022-05-12 16:49:01 +0300

    databse connect done

---
+++ src/utils/connects.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import config from 'config';
+import log from './logger';
+
+const connectDB = async () => {
+    try {
+        const dbUri = config.get<String>("db.dbUri");
+
+        await mongoose.connect(dbUri)
+            .then((response) => {
+                log.info(`MongoDB connection name : ${response.connection.name} on ${response.connection.port}`)
+            })
+
+
+    } catch (error) {
+        log.error("Could not connect to DB : ", error);
+        process.exit(1); // kill program
+    }
+
+}
+
+export default connectDB;
