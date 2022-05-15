beab83add66330b6e1da6de42bbfbe28a830f85c
ciwan <cokyamanmuhammet@gmail.com>
2022-05-15 13:26:42 +0300

    accessToken and refreshToken done

---
+++ src/api/v1/utils/jwt.utils.ts
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+const privateKey = config.get<string>("app.privateKey");
+const publicKey = config.get<string>("app.publicKey");
+
+export const signJwt = (object: Object, options?: jwt.SignOptions | undefined) => {
+    return jwt.sign(object, privateKey, {
+        ...(options && options),
+        algorithm: "RS256"
+    })
+}
+
+export const verifyJwt = (token: string) => {
+    try {
+        const decoded = jwt.verify(token, publicKey);
+
+        return {
+            valid: true,
+            expired: false,
+            decoded
+        }
+    } catch (error: any) {
+        return {
+            valid: false,
+            expired: error.message === "jwt expired",
+            decoded: null
+        }
+    }
+}
