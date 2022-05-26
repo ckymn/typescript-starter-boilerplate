2f0e04b4b760e1da5d6007e84d858b0bc00000d6
ciwan <cokyamanmuhammet@gmail.com>
2022-05-26 17:59:44 +0300

    apicache included

--- server/src/api/v1/routes/index.ts
+++ server/src/api/v1/routes/index.ts
@@ -1,8 +1,14 @@
 import User from './userRoutes';
 import Product from './productRoutes'
 import { Express } from 'express';
+import apicache from 'apicache';
+
+const cache = apicache.middleware;
 
 const routes = (app: Express) => {
+
+    app.use(cache("2 minutes"))
+
     User(app);
     Product(app);
 }
